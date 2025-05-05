d9532f923364caed547e86b04fc26b5876974a6c
Yuki Yoshida <y.yoshida.mars@gmail.com>
2025-05-05 16:17:36 +0900

    ピンアサインの定義ファイルを追加

---
+++ Samples/mini_PF_pins.hpp
@@ -0,0 +1,24 @@
+#ifndef MINI_PF_PINS_HPP
+#define MINI_PF_PINS_HPP
+
+// モータ出力
+#define PIN_MOTOR_L_1 16
+#define PIN_MOTOR_L_2 17
+#define PIN_MOTOR_R_1 18
+#define PIN_MOTOR_R_2 19
+
+// PCとの通信(オプション)
+#define PIN_PC_UART_RX 3
+#define PIN_PC_UART_TX 1
+
+// ラインセンサ
+#define PIN_LINE_SENSOR_1 33
+#define PIN_LINE_SENSOR_2 34
+#define PIN_LINE_SENSOR_3 32
+
+// LED
+#define PIN_LED_RED 21
+#define PIN_LED_YEL 22
+#define PIN_LED_GRN 23
+
+#endif
