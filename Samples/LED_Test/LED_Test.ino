0b1c94e041f4a08e1e77aea265b36940c4a2a25b
Yuki Yoshida <y.yoshida.mars@gmail.com>
2025-05-05 16:17:54 +0900

    LED点灯のテストプログラムを追加

---
+++ Samples/LED_Test/LED_Test.ino
@@ -0,0 +1,49 @@
+//  HWに適したピン定義をインクルードする
+#include "../mini_PF_pins.hpp"
+// #include "../mini_PF_pins_S3.hpp"
+
+void setLED(bool RED,bool YEL,bool GRN){
+    digitalWrite(PIN_LED_RED,RED);
+    digitalWrite(PIN_LED_YEL,YEL);
+    digitalWrite(PIN_LED_GRN,GRN);
+}
+
+void setup(){
+    pinMode(PIN_LED_RED,OUTPUT);
+    pinMode(PIN_LED_YEL,OUTPUT);
+    pinMode(PIN_LED_GRN,OUTPUT);
+
+    delay(100);
+
+    // イニシャル点灯
+    setLED(0,0,1);
+    delay(200);
+    setLED(0,1,0);
+    delay(200);
+    setLED(1,0,0);
+    delay(200);
+    setLED(0,0,0);
+    delay(600);
+
+    setLED(1,1,1);
+    delay(200);
+    setLED(0,0,0);
+    delay(200);
+    setLED(1,1,1);
+    delay(200);
+    setLED(0,0,0);
+    delay(200);
+
+
+}
+
+void loop(){
+    setLED(0,0,1);
+    delay(1000);
+    setLED(0,1,0);
+    delay(1000);
+    setLED(1,0,0);
+    delay(1000);
+    setLED(0,0,0);
+    delay(1000);
+}
