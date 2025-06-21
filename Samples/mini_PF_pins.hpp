6b7d07a27b512bbd5cb61184d343b51c7c5fd32f
Yuki Yoshida <y.yoshida.mars@gmail.com>
2025-06-21 09:25:53 +0900

    ピン定義ファイルを更新

--- Samples/mini_PF_pins.hpp
+++ Samples/mini_PF_pins.hpp
@@ -7,6 +7,12 @@
 #define PIN_MOTOR_R_1 18
 #define PIN_MOTOR_R_2 19
 
+// モータエンコーダ
+#define PIN_MOTOR_L_ENC_A 13
+#define PIN_MOTOR_L_ENC_B 14
+#define PIN_MOTOR_R_ENC_A 25
+#define PIN_MOTOR_R_ENC_B 26
+
 // PCとの通信(オプション)
 #define PIN_PC_UART_RX 3
 #define PIN_PC_UART_TX 1
@@ -14,11 +20,21 @@
 // ラインセンサ
 #define PIN_LINE_SENSOR_1 33
 #define PIN_LINE_SENSOR_2 34
-#define PIN_LINE_SENSOR_3 32
+#define PIN_LINE_SENSOR_3 35
 
 // LED
 #define PIN_LED_RED 21
 #define PIN_LED_YEL 22
 #define PIN_LED_GRN 23
+
+// 汎用IO
+#define PIN_IO0 4
+#define PIN_IO1 27
+#define PIN_IO2 32
+#define PIN_IO3 5
+
+// 電源監視
+#define PIN_VOLTAGE_MONITOR 36
+
 
 #endif
