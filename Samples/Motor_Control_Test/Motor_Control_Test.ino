ff76fa9024733fd5f36aec6625bdc66717b1ceae
Yuki Yoshida <y.yoshida.mars@gmail.com>
2025-07-06 23:53:44 +0900

    モータ制御のサンプルに速度平均を算出する処理を追加

--- Samples/Motor_Control_Test/Motor_Control_Test.ino
+++ Samples/Motor_Control_Test/Motor_Control_Test.ino
@@ -11,10 +11,21 @@
 // https://docs.espressif.com/projects/arduino-esp32/en/latest/api/timer.html
 
 // エンコーダ周りの定義
-volatile int64_t pulse_R = 0;
-volatile int64_t pulse_L = 0;
-volatile int32_t pulse_diff_R = 0;
-volatile int32_t pulse_diff_L = 0;
+
+// 速度を移動平均するための設定
+#define SPEED_BUFFER_LENGTH 5
+const int  SPEED_AVE_WEIGHT[SPEED_BUFFER_LENGTH] = {6, 5, 2, 2, 1}; // 重み付けの設定、総和がビットシフトで演算できる値にする
+const int  SPEED_AVE_DIVISOR = 16; // 重みの総和
+
+volatile  int64_t pulse_R[SPEED_BUFFER_LENGTH];
+volatile  int64_t pulse_L[SPEED_BUFFER_LENGTH];
+volatile  int32_t pulse_diff_R[SPEED_BUFFER_LENGTH];
+volatile  int32_t pulse_diff_L[SPEED_BUFFER_LENGTH];
+volatile  int32_t crnt_pulse_per_microsec_R = 0;
+volatile  int32_t crnt_pulse_per_microsec_L = 0;
+volatile uint16_t pulse_itr = 0;
+
+
 
 volatile uint32_t time_diff = 0xFFFFFFFF;// 意図しない0割りを防ぐため大きな値を初期値とする
 volatile uint64_t last_read_time = 0;
@@ -23,12 +34,8 @@ volatile uint64_t last_read_time = 0;
 
 // カウンタオーバーフロー処理
 #define COUNTER_SKIP_THRESHOLD    3000
-// #define COUNTER_MAX_TO_MIN       10000
 #define COUNTER_MAX               5000
 #define COUNTER_MIN              -5000
-// #define COUNTER_MAX_TO_MIN       65535
-// #define COUNTER_MAX              32767
-// #define COUNTER_MIN             -32768
 
 int16_t cnt_crnt[8] = {0, 0, 0, 0, 0, 0, 0, 0}; 
 int16_t cnt_prv [8] = {0, 0, 0, 0, 0, 0, 0, 0}; 
@@ -55,48 +62,49 @@ int32_t getDiff(pcnt_unit_t unit,byte cnt_no = 0){
 
 // タイマ割り込み周りの定義
 // エンコーダ速度計測
-#define SPEED_TIMER_FREQ  100000
-#define SPEED_TIMER_COUNT   1000
+#define SPEED_CONTROL_TIMER_FREQ  100000
+#define SPEED_CONTROL_TIMER_COUNT   1000
 
-hw_timer_t * speed_timer = NULL;
-volatile SemaphoreHandle_t speed_timer_semaphore;
-portMUX_TYPE speed_timer_mux = portMUX_INITIALIZER_UNLOCKED;
+hw_timer_t * speed_control_timer = NULL;
+volatile SemaphoreHandle_t speed_control_timer_semaphore;
+portMUX_TYPE speed_control_timer_mux = portMUX_INITIALIZER_UNLOCKED;
 
-// 速度制御
-#define CONTROL_TIMER_FREQ  10000
-#define CONTROL_TIMER_COUNT  1000
-hw_timer_t * control_timer = NULL;
-volatile SemaphoreHandle_t control_timer_semaphore;
-portMUX_TYPE control_timer_mux = portMUX_INITIALIZER_UNLOCKED;
-volatile bool exec_control = false;
-
-
-// 速度計測用のタイマ割り込みハンドラ
-void ARDUINO_ISR_ATTR speedTimerInterrupt(){
-    portENTER_CRITICAL_ISR(&speed_timer_mux);
-
-    time_diff = millis() - last_read_time;
-    last_read_time += time_diff;
-    pulse_diff_L = getDiff(PCNT_UNIT_0, 0);
-    pulse_diff_R = getDiff(PCNT_UNIT_1, 1);
-
-    pulse_L += pulse_diff_L;
-    pulse_R += pulse_diff_R;
-
-    portEXIT_CRITICAL_ISR(&speed_timer_mux);
-    xSemaphoreGiveFromISR(speed_timer_semaphore, NULL);
-}
 
 // 速度制御用のタイマ割り込みハンドラ
-void ARDUINO_ISR_ATTR controlTimerInterrupt(){
-    portENTER_CRITICAL_ISR(&control_timer_mux);
+void ARDUINO_ISR_ATTR speedTimerInterrupt(){
+    portENTER_CRITICAL_ISR(&speed_control_timer_mux);
 
-    exec_control = true;
+    // バッファイテレータのインクリメント
+    pulse_itr = (pulse_itr + 1) % SPEED_BUFFER_LENGTH;
 
-    portEXIT_CRITICAL_ISR(&control_timer_mux);
-    xSemaphoreGiveFromISR(control_timer_semaphore, NULL);
+    //  カウンタの値取得
+    time_diff = millis() - last_read_time;
+    last_read_time += time_diff;
+    pulse_diff_L[pulse_itr] = getDiff(PCNT_UNIT_0, 0);
+    pulse_diff_R[pulse_itr] = getDiff(PCNT_UNIT_1, 1);
+
+    pulse_L[pulse_itr] += pulse_diff_L[pulse_itr];
+    pulse_R[pulse_itr] += pulse_diff_R[pulse_itr];
+
+    // 平均速度の計算
+    int32_t L_ave = 0, R_ave = 0;
+
+    for(byte i = 0; i < SPEED_BUFFER_LENGTH; i++) {
+        byte itr_buff =  (pulse_itr + i) % SPEED_BUFFER_LENGTH;
+        L_ave += pulse_diff_L[itr_buff] * SPEED_AVE_WEIGHT[i];
+        R_ave += pulse_diff_R[itr_buff] * SPEED_AVE_WEIGHT[i];
+    }
+    L_ave /= SPEED_AVE_DIVISOR;
+    R_ave /= SPEED_AVE_DIVISOR;
+
+
+    crnt_pulse_per_microsec_L = L_ave * 1000 / (int32_t)time_diff; 
+    crnt_pulse_per_microsec_R = R_ave * 1000 / (int32_t)time_diff; 
+
+
+    portEXIT_CRITICAL_ISR(&speed_control_timer_mux);
+    xSemaphoreGiveFromISR(speed_control_timer_semaphore, NULL);
 }
-
 
 
 // 通信周り
@@ -172,30 +180,23 @@ void setup(){
     pcnt_config.unit           = PCNT_UNIT_1;       //;
     pcnt_config.channel        = PCNT_CHANNEL_1;    //;
     pcnt_unit_config(&pcnt_config);//ユニット初期化;
-;
-;
+
+
     pcnt_counter_pause(PCNT_UNIT_0); //カウンタ一時停止;
-    pcnt_counter_pause(PCNT_UNIT_1);;
+    pcnt_counter_pause(PCNT_UNIT_1);
     pcnt_counter_clear(PCNT_UNIT_0); //カウンタ初期化;
-    pcnt_counter_clear(PCNT_UNIT_1);;
-    Serial.begin(115200);;
+    pcnt_counter_clear(PCNT_UNIT_1);
+    Serial.begin(115200);
     pcnt_counter_resume(PCNT_UNIT_0);// カウント開始;
-    pcnt_counter_resume(PCNT_UNIT_1);;
-;
-;
+    pcnt_counter_resume(PCNT_UNIT_1);
+
+
     // タイマ割り込みのセットアップ;
-    speed_timer_semaphore   = xSemaphoreCreateBinary();;
-    control_timer_semaphore = xSemaphoreCreateBinary();;
-;
-    speed_timer   = timerBegin(SPEED_TIMER_FREQ);;
-    control_timer = timerBegin(CONTROL_TIMER_FREQ);;
-;
-;
-    timerAttachInterrupt(speed_timer  ,&speedTimerInterrupt  );;
-    timerAttachInterrupt(control_timer,&controlTimerInterrupt);;
-;
-    timerAlarm(speed_timer  ,SPEED_TIMER_COUNT  ,true,0);
-    timerAlarm(control_timer,CONTROL_TIMER_COUNT,true,0);
+    speed_control_timer_semaphore = xSemaphoreCreateBinary();
+    speed_control_timer           = timerBegin(SPEED_CONTROL_TIMER_FREQ);
+
+    timerAttachInterrupt(speed_control_timer  ,&speedTimerInterrupt  );
+    timerAlarm(speed_control_timer  ,SPEED_CONTROL_TIMER_COUNT  ,true,0);
 
     //タイマ基準等間隔処理(print)の初期化
     nextPrint = millis() + PRINT_INTERVAL;
@@ -205,24 +206,8 @@ void setup(){
 void loop(){
     if(nextPrint < millis()){{
         
-        
-        sprintf(serialBuff,"L:%9ld ",pulse_L);
-        Serial.print(serialBuff);
-        sprintf(serialBuff,"R:%9ld ",pulse_R);
-        Serial.print(serialBuff);
-        sprintf(serialBuff,"/ SPEED L:%5d R:%5d ",pulse_diff_L,pulse_diff_R);
-        Serial.print(serialBuff);
-        Serial.println();
 
-        // Serial.print(pulse_L);
-        // Serial.print(",");
-        // Serial.print(pulse_R);
-        // Serial.print(",");
-        // Serial.print(pulse_diff_L);
-        // Serial.print(",");
-        // Serial.println(pulse_diff_R);
-
-
+        //  生値の表示
         // Serial.print(cnt_crnt[0]);
         // Serial.print(",");
         // Serial.print(cnt_crnt[1]);
@@ -230,6 +215,24 @@ void loop(){
         // Serial.print(cnt_prv[0]);
         // Serial.print(",");
         // Serial.println(cnt_prv[1]);
+
+        // 平均計算前のエンコーダ値と速度
+        // Serial.print(pulse_L[pulse_itr]);
+        // Serial.print(",");
+        // Serial.print(pulse_R[pulse_itr]);
+        // Serial.print(",");
+        // Serial.print(pulse_diff_L[pulse_itr]);
+        // Serial.print(",");
+        // Serial.println(pulse_diff_R[pulse_itr]);
+
+
+        // 平均計算後の速度
+        Serial.print("ave p/ms L : ");
+        Serial.print(crnt_pulse_per_microsec_L);
+        Serial.print(",R");
+        Serial.print(crnt_pulse_per_microsec_R);
+        Serial.println();
+
     
     }
         nextPrint = millis() + PRINT_INTERVAL;
